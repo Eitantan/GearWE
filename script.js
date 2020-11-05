02c668fe63a5ea5fde29f96244372c4555ddca05
Eitantan <56890550+Eitantan@users.noreply.github.com>
2020-11-04 18:25:35 -0700

    1.2

--- script.js
+++ script.js
@@ -29,24 +29,7 @@ function link(txt,href) {
 	link.innerText = txt
 	return link
 }
-//Test Code
-// element("Hello GearWE!", "h1")
-// element("Hello GearWE!", "h2")
-// element("Hello GearWE!", "h3")
-// element("Hello GearWE!", "h4")
-// element("Hello GearWE!", "h5")
-// element("Hello GearWE!", "h6")
-// element("Hello GearWE!", "b")
-// newline()
-// element("Hello GearWE!", "i")
-// newline()
-// element("Hello GearWE!", "u")
-// newline()
-// element("Hello GearWE!", "strike")
-// newline()
-//  button("Insert Picture", function(){
-// 	let ans = prompt("Insert Picture Code. (Remember to add https:// at the front)")
-//    	let width = prompt("Width:")
-// 	  let height = prompt("Height:")
-//   	img(ans, width, height)
-//  })
+function plaintext(txt) {
+	txt.replace(/</g, "&lt;")
+	txt.replace(/>/g, "&gt;")
+}
